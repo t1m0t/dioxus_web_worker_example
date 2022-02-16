4eec0ae1d2246e5dc135fa810cc27fc875ee1980
t1m0t <t1m95.tb@gmail.com>
2022-02-16 18:07:06 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::rc::Rc;
 
 use dioxus::prelude::*;
 use gloo_worker::Bridged;
+use serde::__private::de::IdentifierDeserializer;
 
 mod agent;
 use crate::agent::{ExampleWorker, ExampleWorkerInput};
@@ -14,16 +15,25 @@ fn main() {
 }
 
 pub fn App(cx: Scope) -> Element {
+    let (result, set_result) = use_state(&cx, || 0 as u32);
     let mut worker = ExampleWorker::bridge(Rc::new(move |e| log::info!("{:?}", e)));
+    let (input, set_input) = use_state(&cx, move || 0 as u32);
     cx.render(rsx! (
-        div { "Hello, world!" }
+        div { "Fibonacci example" }
+        input {
+            r#type: "text",
+            value: "0",
+            oninput: move |e| {
+                set_input(e.value)
+            }
+        }
         button {
-            onclick: move |_| {
+            onclick: move |e| {
                 worker.send(ExampleWorkerInput {
                     n: 10
-                })
+                });
             },
-            "click here "
+            "click me"
         }
     ))
 }
