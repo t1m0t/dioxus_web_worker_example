dbf03c34154e174a762276dc8a9d31ef97e494cb
t1m0t <t1m95.tb@gmail.com>
2022-02-17 14:34:33 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #![allow(non_snake_case)]
-use std::{rc::Rc, cell::RefCell};
+use std::{
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
+    fmt::{self},
+    rc::Rc,
+};
 
 use dioxus::prelude::*;
 use gloo_worker::Bridged;
@@ -13,13 +18,44 @@ fn main() {
     dioxus::web::launch(App);
 }
 
+struct ComputedValue<T> {
+    value: RefCell<T>,
+}
+
+impl<T> fmt::Display for ComputedValue<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.value.borrow())
+    }
+}
+
+impl<T> ComputedValue<T> {
+    fn new(val: T) -> Self {
+        Self {
+            value: RefCell::new(val),
+        }
+    }
+}
+
 pub fn App(cx: Scope) -> Element {
-    let val = Rc::new(RefCell::new(0 as u32) );
+    let val = Rc::new(ComputedValue::new(0 as u32));
+    let status = Rc::new(ComputedValue::new(false));
     let (result, set_result) = use_state(&cx, || 0 as u32);
-    let mut worker = ExampleWorker::bridge(Rc::new(move |e| { *val.borrow_mut() = e.value} ));
+
+    let mut worker = ExampleWorker::bridge(Rc::new({
+        let inside_val = val.clone();
+        let status = status.clone();
+        move |e| {
+            *inside_val.value.borrow_mut() = e.value;
+            *status.value.borrow_mut() = true;
+        }
+    }));
+
     let (input, set_input) = use_state(&cx, move || 0 as u32);
     let (error, set_error) = use_state(&cx, move || false);
-    let (error_message, set_error_message) = use_state(&cx, move|| "");
+    let (error_message, set_error_message) = use_state(&cx, move || "");
 
     let input_message = if *error {
         set_error_message("error: input should be between 1 and 50");
@@ -27,13 +63,13 @@ pub fn App(cx: Scope) -> Element {
             "{error_message}"
         })
     } else {
-        set_input(*val.borrow());
         cx.render(rsx! {
             "entered value {input}"
         })
     };
 
-    let result_message = if *result > 0 {
+    let result_message = if *status.value.borrow_mut() {
+        set_result(*val.value.borrow_mut());
         cx.render(rsx! {
             "result of computation is: {result}"
         })
@@ -55,7 +91,8 @@ pub fn App(cx: Scope) -> Element {
                             if *error {set_error(false)} else {};
                             set_input(val)
                         } else {
-                            set_error(true)
+                            set_error(true);
+                            set_input(0)
                         }
                     },
                     Err(_) => {()}
@@ -71,10 +108,13 @@ pub fn App(cx: Scope) -> Element {
             "click me to compute"
         }
         div {
-            input_message            
+            input_message
         }
         div {
             result_message
+        }
+        div {
+            "{val}"
         }
     ))
 }
